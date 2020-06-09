33a410ee695bc7f0427d1ff4a93a7fcfed7f36bb
jiazhiguang <jia_zhiguang@126.com>
2020-06-09 14:57:12 +0800

    execute sgx_sign in the occlum container

--- shim/runtime/carrier/constants/constants.go
+++ shim/runtime/carrier/constants/constants.go
@@ -89,45 +89,170 @@ function start() {
 
 start $@`
 
-	//FIXME
-	BuildOcclumEnclaveScript = `#!/bin/bash
-		set -xe
-		data_dir=/data
-		rootfs=/rootfs
-		work_dir=%s
-		entry_point=%s
-		occlum_config_path=${rootfs}/%s
-		occlum_workspace=${data_dir}/../occlum_workspace
-		rm -fr ${occlum_workspace}
-		mkdir -p ${occlum_workspace}
-		pushd ${occlum_workspace}
-		occlum init
-		if [[ "${occlum_config_path}" != "" && -f ${occlum_config_path} ]];then
-			/bin/cp -f ${occlum_config_path} Occlum.json
-		fi
-		sed -i "s#/bin#${entry_point}#g" Occlum.json
-		/bin/bash ${data_dir}/replace_occlum_image.sh ${rootfs} image
-		occlum build
-		rm -f ${rootfs}/${work_dir}/.occlum/build/lib/libocclum-libos.signed.so
-		mkdir -p ${rootfs}/${work_dir} || true
-		/bin/cp -fr .occlum ${rootfs}/${work_dir}
-        # ===fixme debug====
-        /bin/cp -fr image ${rootfs}/${work_dir}
-        /bin/cp -f Occlum.json ${rootfs}/${work_dir}
-        # ==================
-		/bin/cp -f Enclave.xml ${data_dir}
-		popd
-		pushd ${rootfs}/${work_dir}
-		# ==== copy sgxsdk libs =======
-		lib_dir=${rootfs}/lib
-		/bin/cp -f /usr/lib/x86_64-linux-gnu/libprotobuf.so ${lib_dir}
-		/bin/cp -f /lib/x86_64-linux-gnu/libseccomp.so.2 ${lib_dir}
-		/bin/cp -f /usr/lib/libsgx_u*.so* ${lib_dir}
-		/bin/cp -f /usr/lib/libsgx_enclave_common.so.1 ${lib_dir} 
-		/bin/cp -f /usr/lib/libsgx_launch.so.1 ${lib_dir}
-		# ==================
-		ln -sfn .occlum/build/lib/libocclum-pal.so liberpal-occlum.so
-		chroot ${rootfs} /sbin/ldconfig
-		popd
-		`
+	CarrierScript = `#!/bin/bash
+set -xe
+base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+occlum_workspace=/occlum_workspace
+
+temp=$(getopt -a -o a:r:w:p:c:e:u:m:s:k:n: -l action:,rootfs:,work_dir:,entry_point:,occlum_config_path:,enclave_config_path:,\
+unsigned_encalve_path:,unsigned_material_path:,signed_enclave_path:,public_key_path:,signature_path: -- "$@")
+eval set -- "$temp"
+
+while true
+do
+  case "$1" in
+    -a|--action)
+      action=$2; shift;;
+    -r|--rootfs)
+      rootfs=$2; shift;;
+    -w|--work_dir)
+      work_dir=$2; shift;;
+    -p|--entry_point)
+      entry_point=$2; shift;;
+    -c|--occlum_config_path)
+      occlum_config_path=$2; shift;;
+    -e|--enclave_config_path)
+      enclave_config_path=$2; shift;;
+    -u|--unsigned_encalve_path)
+      unsigned_encalve_path=$2; shift;;
+    -m|--unsigned_material_path)
+      unsigned_material_path=$2; shift;;
+    -s|--signed_enclave_path)
+      signed_enclave_path=$2; shift;;
+    -k|--public_key_path)
+      public_key_path=$2; shift;;
+    -n|--signature_path)
+      signature_path=$2; shift;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Unknown argument: $1"; shift;;
+  esac
+shift
+done
+
+function copyOcclumLiberaries() {
+  pushd ${rootfs}/${work_dir}
+  local lib_dir=${rootfs}/lib
+  /bin/cp -f /usr/lib/x86_64-linux-gnu/libprotobuf.so ${lib_dir}
+  /bin/cp -f /lib/x86_64-linux-gnu/libseccomp.so.2 ${lib_dir}
+  /bin/cp -f /usr/lib/libsgx_u*.so* ${lib_dir}
+  /bin/cp -f /usr/lib/libsgx_enclave_common.so.1 ${lib_dir}
+  /bin/cp -f /usr/lib/libsgx_launch.so.1 ${lib_dir}
+  ln -sfn .occlum/build/lib/libocclum-pal.so liberpal-occlum.so
+  # ==== fixme: the file /sbin/ldconfig maybe not exist in customer's image
+  chroot ${rootfs} /sbin/ldconfig
+  popd
+}
+
+function buildUnsignedEnclave(){
+  if [[ "${entry_point}" == "" || "${rootfs}" == "" || "${work_dir}" == "" ]]; then
+    echo "BuildUnsignedEnclave:: the argumentes should not be empty: entry_point, rootfs, work_dir"
+    exit 1
+  fi
+
+  rm -fr ${occlum_workspace}
+  mkdir -p ${occlum_workspace}
+  pushd ${occlum_workspace}
+  # occlum init
+  occlum init
+  # replace Occlum.json with user-supplied Occlum.json
+  if [[ "${occlum_config_path}" != "" && -f ${occlum_config_path} ]];then
+    /bin/cp -f ${occlum_config_path} Occlum.json
+  fi
+  # set occlum entrypoint
+  sed -i "s#/bin#${entry_point}#g" Occlum.json
+  # build occlum image
+  /bin/bash ${base_dir}/replace_occlum_image.sh ${rootfs} image
+  # occlum build
+  occlum build
+  mkdir -p ${rootfs}/${work_dir} || true
+  /bin/cp -fr .occlum ${rootfs}/${work_dir}
+  /bin/cp -f Enclave.xml ${rootfs}/${work_dir}
+  # ===fixme debug====
+  /bin/cp -fr image ${rootfs}/${work_dir}
+  /bin/cp -f Occlum.json ${rootfs}/${work_dir}
+  # ==================
+  # copy occlum liberaries to rootfs
+  copyOcclumLiberaries
+  popd
+}
+
+function generateSigningMaterial() {
+  if [[ "${enclave_config_path}" == "" || "${unsigned_encalve_path}" == "" || "${unsigned_material_path}" == ""  ]]; then
+    echo "GenerateSigningMaterial:: the argumentes should not be empty: enclave_config_path, unsigned_encalve_path, unsigned_material_path"
+    exit 1
+  fi
+  /opt/intel/sgxsdk/bin/x64/sgx_sign gendata -enclave ${unsigned_encalve_path} -config ${enclave_config_path} -out ${unsigned_material_path}
+}
+
+function cascadeEnclaveSignature() {
+  if [[ "${enclave_config_path}" == "" || "${unsigned_encalve_path}" == "" || "${unsigned_material_path}" == "" \
+	|| "${signed_enclave_path}" == "" || "${public_key_path}" == "" || "${signature_path}" == "" ]]; then
+    echo "CascadeEnclaveSignature:: the argumentes should not be empty: enclave_config_path, unsigned_encalve_path, unsigned_material_path, signed_enclave_path, public_key_path, signature_path"
+    exit 1
+  fi
+  /opt/intel/sgxsdk/bin/x64/sgx_sign catsig -enclave ${unsigned_encalve_path} -config ${enclave_config_path} -out ${signed_enclave_path} -key ${public_key_path} \
+    -sig ${signature_path} -unsigned ${unsigned_material_path}
+}
+
+function mockSignature() {
+  if [[ "${public_key_path}" == "" || "${signature_path}" == "" || "${unsigned_material_path}" == "" ]]; then
+    echo "MockSignature:: the argumentes should not be empty: public_key_path, signature_path, unsigned_material_path"
+    exit 1
+  fi
+  dir=$(mktemp -d)
+  openssl genrsa -out ${dir}/privatekey.pem -3 3072
+  openssl rsa -in ${dir}/privatekey.pem -pubout -out ${public_key_path}
+  openssl dgst -sha256 -out ${signature_path} -sign ${dir}/privatekey.pem -keyform PEM ${unsigned_material_path}
+  rm -fr ${dir}
+}
+
+function doAction(){
+  if [ "${action}" == "" ]; then
+    echo "the argument should not be empty: action"
+    exit 1
+  fi
+  case ${action} in
+    buildUnsignedEnclave)
+      buildUnsignedEnclave
+      ;;
+    generateSigningMaterial)
+      generateSigningMaterial
+      ;;
+    cascadeEnclaveSignature)
+      cascadeEnclaveSignature
+      ;;
+    mockSignature)
+      mockSignature
+      ;;
+    *)
+      echo "unknown action: ${action}"
+      exit 1
+      ;;
+  esac
+}
+
+doAction`
+
+	StartScript = `#!/bin/bash
+function handle_TERM() {
+  echo "recevied signal SIGTERM, exit now"
+  exit 0
+}
+
+function handle_INT() {
+  echo "recevied signal SIGINT, exit now"
+  exit 0
+}
+
+trap 'handle_INT' SIGINT
+trap 'handle_TERM' SIGTERM
+
+while true
+do
+  sleep 1
+done`
 )
