77613e51d3f1e477e3de7ddbf4c77be645b045ca
Jia Zhang <zhang.jia@linux.alibaba.com>
2020-06-23 13:26:44 +0000

    rune/libenclave: Implement the general sgx entry point for skeleton
    In order to implement more ecall functions, it is necessary to
    implement a general sgx entry point with ecall number to
    corresponding ecall handler.
    
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- rune/libenclave/internal/runtime/pal/skeleton/sgx_call.h
+++ rune/libenclave/internal/runtime/pal/skeleton/sgx_call.h
@@ -6,6 +6,36 @@
 #ifndef SGX_CALL_H
 #define SGX_CALL_H
 
-void sgx_call_eenter(void *rdi, void *rsi, void *entry);
+#define ECALL_MAGIC		0
+#define MAX_ECALLS		1
+
+#define EEXIT			4
+
+#define INIT_MAGIC		0xdeadfacedeadbeefUL
+
+#ifndef __ASSEMBLER__
+
+#define SGX_ENTER_1_ARG(ecall_num, tcs, a0) \
+	({      \
+		int __ret; \
+		asm volatile( \
+			"mov %1, %%r10\n\t" \
+			"mov %2, %%r11\n\t" \
+			"call sgx_ecall\n\t" \
+			: "=a" (__ret) \
+			: "r" ((uint64_t)ecall_num), "r" (tcs), \
+			  "D" (a0) \
+			: "r10", "r11" \
+		); \
+		__ret; \
+	})
+
+#define ENCLU			".byte 0x0f, 0x01, 0xd7"
+
+#else
+
+#define ENCLU			.byte 0x0f, 0x01, 0xd7
+
+#endif
 
 #endif /* SGX_CALL_H */
