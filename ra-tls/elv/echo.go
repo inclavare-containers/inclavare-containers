da500b75ce202285cc3817fb68f6c720ba80c106
Jia Zhang <zhang.jia@linux.alibaba.com>
2020-09-17 03:31:21 +0000

    ra-tls: Update the repo
    - Introduce elv used for the communication with ra-tls-server
    - ra-tls-server now is moved out from wolfssl-examples
    - Remove wolfssl-examples in build process
    
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

---
+++ ra-tls/elv/echo.go
@@ -0,0 +1,65 @@
+package main
+
+/*
+#cgo CFLAGS: -I../build/include -I/opt/intel/sgxsdk/include -I../sgx-ra-tls
+#cgo LDFLAGS: -L../build/lib -l:libra-challenger.a -l:libwolfssl.a -lm
+
+extern int ra_tls_echo(int sockfd);
+*/
+import "C"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"net"
+)
+
+const (
+	defaultAddress = "/run/rune/ra-tls.sock"
+)
+
+var echoCommand = cli.Command{
+	Name:  "echo",
+	Usage: "echo the message",
+	ArgsUsage: `[command options]
+
+EXAMPLE:
+
+       # shelter attest foo.com`,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "addr",
+			Usage: "ra-tls server address",
+		},
+		cli.StringFlag{
+			Name:  "port",
+			Usage: "ra-tls server port",
+		},
+	},
+	SkipArgReorder: true,
+	Action: func(cliContext *cli.Context) error {
+		addr := cliContext.String("addr")
+		if addr == "" {
+			addr = defaultAddress
+		}
+
+		conn, err := net.Dial("unix", addr)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		unixConn, ok := conn.(*net.UnixConn)
+		if !ok {
+			return fmt.Errorf("casting to UnixConn failed")
+		}
+
+		sockfd, err := unixConn.File()
+		if err != nil {
+			return err
+		}
+
+		C.ra_tls_echo(C.int(sockfd.Fd()))
+
+		return nil
+	},
+}
