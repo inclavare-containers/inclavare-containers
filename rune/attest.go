cd6f18835f485c96f02290bc83739936252f73cd
YiLin.Li <YiLin.Li@linux.alibaba.com>
2020-09-28 15:09:18 +0000

    rune/attest.go: both support remote or local report in  `rune attest` command
    Signed-off-by: Yilin Li <YiLin.Li@linux.alibaba.com>

--- rune/attest.go
+++ rune/attest.go
@@ -21,10 +21,14 @@ const (
 
 var attestCommand = cli.Command{
 	Name:  "attest",
-	Usage: "attest generates a remote attestation to the corresponding enclave container",
+	Usage: "attest gets the remote or local report to the corresponding enclave container",
 	ArgsUsage: `<container-id> [command options]
 Where "<container-id>" is the name for the instance of the container`,
 	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "isRA",
+			Usage: "specify whether to get the remote or local report",
+		},
 		cli.BoolFlag{
 			Name:  "product",
 			Usage: "specify whether using production attestation service",
@@ -133,6 +137,12 @@ func getAttestProcess(context *cli.Context, bundle string) (*specs.Process, erro
 		}
 	}
 	// append the passed env variables
+	isRemoteAttestation := "false"
+	if context.Bool("isRA") {
+		isRemoteAttestation = "true"
+	}
+	p.Env = append(p.Env, "IsRemoteAttestation"+envSeparator+isRemoteAttestation)
+
 	p.Env = append(p.Env, "SPID"+envSeparator+context.String("spid"))
 	p.Env = append(p.Env, "SUBSCRIPTION_KEY"+envSeparator+context.String("subscription-key"))
 
