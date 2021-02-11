ba22581117ed972a2f95af9fd66d9e780cf51d34
YiLin.Li <YiLin.Li@linux.alibaba.com>
2021-02-11 16:02:36 +0800

    rune/libenclave/attestation: add attester interface and support to both sgx ecdsa and epid attesters
    Fixes: #620
    Signed-off-by: Yilin Li <YiLin.Li@linux.alibaba.com>

---
+++ rune/libenclave/attestation/attester.go
@@ -0,0 +1,29 @@
+package attestation // import "github.com/inclavare-containers/rune/libenclave/attestation"
+
+import (
+	"fmt"
+	"github.com/inclavare-containers/rune/libenclave/attestation/registration"
+	_ "github.com/inclavare-containers/rune/libenclave/attestation/sgx/attester" // for the registration of sgx attesters
+)
+
+type Attester interface {
+	Name() string
+	New(map[string]string) error
+	GetTargetInfo() ([]byte, error)
+	GetQuote([]byte) ([]byte, error)
+}
+
+func NewAttester(aType string, cfg map[string]string) (Attester, error) {
+	for _, attester := range registration.AttesterRegisterationList {
+		a := attester.Registeration.(Attester)
+		if a.Name() == aType {
+			if err := a.New(cfg); err != nil {
+				return nil, err
+			}
+
+			return a, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Unsupported attestation service %s specified", aType)
+}
