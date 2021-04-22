98d8c3503b224901df1b11a0e4f4c747be8c7d32
haosanzi <2277416933@qq.com>
2021-04-22 15:30:15 +0800

    enclave-tls/samples: fix return value error (#820)
    Signed-off-by: Shirong Hao <shirong@linux.alibaba.com>

--- enclave-tls/samples/enclave-tls-client/client.c
+++ enclave-tls/samples/enclave-tls-client/client.c
@@ -113,14 +113,13 @@ int enclave_tls_echo(int fd, enclave_tls_log_level_t log_level,
 		printf("Server: %s\n", buf);
 	}
 
+	return 0;
+
 err:
 	ret = enclave_tls_cleanup(handle);
-	if (ret != ENCLAVE_TLS_ERR_NONE) {
+	if (ret != ENCLAVE_TLS_ERR_NONE)
 		fprintf(stderr, "failed to cleanup %#x\n", ret);
-		return -1;
-	}
-
-	return ret;
+	return -1;
 }
 
 int main(int argc, char **argv)
