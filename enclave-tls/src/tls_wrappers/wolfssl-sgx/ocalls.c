3fae2ed6f0b83dc064da244769f43dd62dcc59c4
Jia Zhang <zhang.jia@linux.alibaba.com>
2021-04-25 06:49:40 +0000

    enclave-tls/wolfssl-sgx: print the debug message with ETLS_DEBUG
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- enclave-tls/src/tls_wrappers/wolfssl-sgx/ocalls.c
+++ enclave-tls/src/tls_wrappers/wolfssl-sgx/ocalls.c
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <enclave-tls/log.h>
 #include "../wolfssl/oid.c"
 #include "../wolfssl/un_negotiate.c"
 
@@ -18,6 +19,6 @@ size_t ocall_send(int sockfd, const void *buf, size_t len, int flags)
 
 int ocall_verify_certificate(uint8_t *der_crt, uint32_t der_crt_len)
 {
-	printf ("der_crt  %p, der_crt_len %d\n", der_crt, der_crt_len);
+	ETLS_DEBUG("der_crt %p, der_crt_len %d\n", der_crt, der_crt_len);
 	return verify_certificate(der_crt, der_crt_len);
 }
