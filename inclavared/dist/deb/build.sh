a3152872ea3e7205706438a518cce0e6f8f8ffa0
YiLin.Li <YiLin.Li@linux.alibaba.com>
2021-05-15 00:11:06 +0800

    inclavared/dist: add the rustc anc cargo check in the build.sh rather than in the build-depends section for the deb package
    Fixes: #978
    Signed-off-by: Yilin Li <YiLin.Li@linux.alibaba.com>

--- inclavared/dist/deb/build.sh
+++ inclavared/dist/deb/build.sh
@@ -22,8 +22,13 @@ cd $DEBBUILD_DIR && tar zcfP $TARBALL_NAME $PACKAGE-$VERSION
 
 # check the Rust
 if ! [ -x "$(command -v rustc)" ]; then
-  echo 'Error: Rust is not installed. Please install Rust firstly'
+  echo 'Error: Rust is not installed. Please type the "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y; source $HOME/.cargo/env" command to install Rust firstly'
   exit 1
+fi
+
+if ! [ -x "$(command -v cargo)" ]; then
+   echo 'Error: Cargo is not installed. Please type the "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y; source $HOME/.cargo/env" command to install Cargo firstly'
+   exit 1
 fi
 
 # build_deb_package
