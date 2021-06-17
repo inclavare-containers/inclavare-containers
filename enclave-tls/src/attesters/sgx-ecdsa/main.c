bd00e06dec7565942d013a3d41728eb6de9078f9
Shirong Hao <shirong@linux.alibaba.com>
2021-06-17 15:23:59 +0000

    enclave-tls: update copyright information
    Signed-off-by: Shirong Hao <shirong@linux.alibaba.com>

--- enclave-tls/src/attesters/sgx-ecdsa/main.c
+++ enclave-tls/src/attesters/sgx-ecdsa/main.c
@@ -1,4 +1,5 @@
-/* Copyright (c) 2020-2021 Alibaba Cloud and Intel Corporation
+/* Copyright (c) 2021 Intel Corporation
+ * Copyright (c) 2020-2021 Alibaba Cloud
  *
  * SPDX-License-Identifier: Apache-2.0
  */
