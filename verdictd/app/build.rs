29e56301eaa1864d57ba9a0cf908bc543bc11dab
EmisonLu <lzh1633856298@gmail.com>
2021-08-02 16:27:19 +0800

    verdictd/opa: solve .so link problems and go package installation problems

--- verdictd/app/build.rs
+++ verdictd/app/build.rs
@@ -1,10 +1,12 @@
 use std::process::Command;
+use std::env;
+use std::path::Path;
 
 fn main() {
     println!("cargo:rustc-link-search=native=/opt/enclave-tls/lib");
     println!("cargo:rustc-link-lib=dylib=enclave_tls");
 
-    println!(r"cargo:rustc-link-search=target/debug");
+    println!("cargo:rustc-link-search=native=./src/policyEngine/opa");
     let os = Command::new("uname").output().unwrap();
     let ext = match String::from_utf8_lossy(os.stdout.as_slice())
         .into_owned()
@@ -14,16 +16,23 @@ fn main() {
         "Darwin" => "dylib",
         _ => "so",
     };
+
+    let root = Path::new("./src/policyEngine/opa");
+    env::set_current_dir(&root).unwrap();
+
     Command::new("go")
         .args(&[
             "build",
             "-o",
-            &format!("target/debug/libopa.{}", ext),
+            &format!("libopa.{}", ext),
             "-buildmode=c-shared",
-            "src/policyEngine/opa/opaEngine.go",
+            "opaEngine.go",
         ])
         .status()
         .unwrap();
+
+    let root = Path::new("../../../");
+    env::set_current_dir(&root).unwrap();
 
     tonic_build::compile_protos("proto/keyprovider.proto");
 }
