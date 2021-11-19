80c0de57aecd2dceb9a9cfbff77f6a956540d2c3
Jia Zhang <zhang.jia@linux.alibaba.com>
2021-11-19 05:13:03 +0000

    rats-tls: fix auto-selection sgx_ecdsa verifier in non-sgx platform
    For example, tdx architecture doesn't support sgx in guest TD. However,
    rats-tls still automatically selects sgx_ecdsa verifier instance by
    default due to the lack of checking sgx2 support in guest TD.
    
    In addition, the cpu capability check shoud go the core layer in
    rats-tls. This design inspires the commit
    9796454c89083fdd69170f709e95d5342b04388f.
    
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- rats-tls/src/include/internal/cpu.h
+++ rats-tls/src/include/internal/cpu.h
@@ -4,16 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef _INTERNAL_CPU_H
+#define _INTERNAL_CPU_H
+
 #include <stdbool.h>
 
 #define SGX_CPUID 0x12
 #define TDX_CPUID 0x21
 
-#define SGX1_STRING     0x00000001
-#define SGX2_STRING     0x00000002
+#define SGX1_STRING	0x00000001
+#define SGX2_STRING	0x00000002
 #define TDX_STRING_HIGH 0x65746e49
-#define TDX_STRING_LOW  0x5844546c
+#define TDX_STRING_LOW	0x5844546c
 
 extern bool is_sgx1_supported(void);
 extern bool is_sgx2_supported(void);
 extern bool is_in_tdguest(void);
+
+#endif /* _INTERNAL_CPU_H */
