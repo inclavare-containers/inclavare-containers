5f758af5b7b678d8a40f79aa0269d7fa7eecfb50
liangzhou121 <liang.a.zhou@linux.alibaba.com>
2021-12-21 14:27:01 +0800

    Verdictd: update OPA to support different TEEs (#1527)
    1.Enhance OPA interfaces
    
    Signed-off-by: Zhou Liang <liang.a.zhou@linux.alibaba.com>

--- verdictd/src/policy_engine/opa/opa_engine.go
+++ verdictd/src/policy_engine/opa/opa_engine.go
@@ -5,63 +5,68 @@ import "C"
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"log"
 	"strings"
 
 	"github.com/open-policy-agent/opa/rego"
+	"github.com/open-policy-agent/opa/storage/inmem"
 )
 
 //export makeDecisionGo
-func makeDecisionGo(policy string, message string) *C.char {
-
+func makeDecisionGo(policy string, data string, input string) *C.char {
 	// Deserialize the message in json format
-	message_map := make(map[string]interface{})
-	err := json.Unmarshal([]byte(message), &message_map)
+	input_map := make(map[string]interface{})
+	err := json.Unmarshal([]byte(input), &input_map)
 	if err != nil {
-		log.Fatal(err)
-		return C.CString("")
+		return C.CString("Unmarshal input error.")
 	}
+
+	data_map := make(map[string]interface{})
+	err2 := json.Unmarshal([]byte(data), &data_map)
+	if err2 != nil {
+		// Handle error.
+		return C.CString("Unmarshal data error.")
+	}
+	// Manually create the storage layer. inmem.NewFromObject returns an
+	// in-memory store containing the supplied data.
+	store := inmem.NewFromObject(data_map)
 
 	// Construct a Rego object that can be prepared or evaluated.
 	r := rego.New(
 		rego.Query("input;data.policy"),
 		rego.Module("demo.rego", policy),
+		rego.Store(store),
 	)
 
 	// Create a prepared query that can be evaluated.
 	ctx := context.Background()
 	query, err := r.PrepareForEval(ctx)
 	if err != nil {
-		log.Fatal(err)
-		return C.CString("")
+		return C.CString(err.Error())
 	}
 
 	// Make opa query
-	rs, err := query.Eval(ctx, rego.EvalInput(message_map))
+	rs, err := query.Eval(ctx, rego.EvalInput(input_map))
 	if err != nil {
-		log.Fatal(err)
-		return C.CString("")
+		return C.CString(err.Error())
 	}
 
 	// Transform the processed decision into the format rust hopes for
-	input := rs[0].Expressions[0].Value.(map[string]interface{})
-	dataDemo := rs[0].Expressions[1].Value.(map[string]interface{})
+	inputOPA := rs[0].Expressions[0].Value.(map[string]interface{})
+	dataOPA := rs[0].Expressions[1].Value.(map[string]interface{})
 	parseInfo := make(map[string]interface{})
 
-	for k, v := range input {
-		value := [2]interface{}{v, dataDemo[k]}
+	for k, v := range inputOPA {
+		value := [2]interface{}{v, data_map[k]}
 		parseInfo[k] = value
 	}
 
 	decisionMap := make(map[string]interface{})
 	decisionMap["parseInfo"] = parseInfo
-	decisionMap["allow"] = dataDemo["allow"]
+	decisionMap["allow"] = dataOPA["allow"]
 
 	decision, err := json.Marshal(decisionMap)
 	if err != nil {
-		fmt.Println("json.Marshal failed: ", err)
-		return C.CString("")
+		return C.CString("Unmarshal decision error.")
 	}
 	decision_str := string(decision)
 	res := strings.Replace(decision_str, "\\u003e", ">", -1)
